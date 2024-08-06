f6b356489f8e626f5db73d798dab928411f101c9
spenserfer01 <spenserfer@gmail.com>
2024-08-06 05:23:05 -0400

    agrear segunda actualizacion

--- primer contenido/prueba.py
+++ primer contenido/prueba.py
@@ -3,8 +3,6 @@ from zeep import Client
 # Crear el cliente con la URL del WSDL
 client = Client("https://www.dataaccess.com/webservicesserver/NumberConversion.wso?WSDL")
 
-# Imprimir los servicios y métodos disponibles
-print(client.service)
 
 # Llamar al método del servicio web
 result = client.service.NumberToWords(5)
